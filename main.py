a30c9ffe1430c742f861aec57f722c78de399aab
Sally0604 <yp11332141@yphs.tp.edu.tw>
2025-11-13 22:11:51 +0800

    tutorial about installing pygame added

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
+# 1. go to "cmd" and input "python"
+# 2. it will ask you to install python, just install
+# 3. after installation, input "pip install pygame"
+# 4. there you go
 import pygame # finally!!! ahhahhaaahah
